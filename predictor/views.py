5a42abbda92f7e52953c13651f2db0a017db8988
Victor Mwandike <mwandikev@gmail.com>
2025-02-13 09:04:56 +0300

    Added changes

--- predictor/views.py
+++ predictor/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(response):
-    return render(response, "accounts/home.html")
+    return render(response, "features/home.html")
 
 def risk_assessment(response):
-    return render(response, "accounts/risk_assessment.html")
+    return render(response, "features/risk_assessment.html")
 
 # Create your views here.
