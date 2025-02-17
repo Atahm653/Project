6049bf5cee93d122159a3b8a03a299dd04dda549
Victor Mwandike <mwandikev@gmail.com>
2025-02-17 11:46:11 +0300

    Added changes

--- predictor/urls.py
+++ predictor/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("risk", views.risk_assessment, name="risk"),
-    path("login", views.login, name="login"),
     path("register", views.register, name="register"),
 ]
--- predictor/views.py
+++ predictor/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(response):
@@ -8,10 +10,15 @@ def home(response):
 def risk_assessment(response):
     return render(response, "features/risk_assessment.html")
 
-def login(response):
-    return render(response, "features/login.html")
-
 def register(response):
-    return render(response, "features/register.html")
+    if response.method == "POST":
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            form.save()
+        return redirect ("/home")
+    else:
+       form = UserCreationForm()
+       
+    return render(response, "registration/register.html", {"form":form})
 
 # Create your views here.
