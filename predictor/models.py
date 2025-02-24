8d5723cfe39e8c034236de50246e850e385c9676
Victor Mwandike <mwandikev@gmail.com>
2025-02-24 10:10:56 +0300

    New changes

--- predictor/models.py
+++ predictor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    age = models.IntegerField(max_length=3, null=True)
+    gender = models.CharField(max_length=10, null=True)
+    email = models.EmailField(max_length=100, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
