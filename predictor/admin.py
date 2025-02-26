5e28f36f2a9f37bc183d3e75a3de524b3c11204e
Victor Mwandike <mwandikev@gmail.com>
2025-02-26 21:56:19 +0300

    Added changes

--- predictor/admin.py
+++ predictor/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Patient
+from .models import Prediction
+from .models import RiskAssessment
 
 admin.site.register(Patient)
+admin.site.register(Prediction)
+admin.site.register(RiskAssessment)
--- predictor/models.py
+++ predictor/models.py
@@ -11,3 +11,28 @@ class Patient(models.Model):
 
     def __str__(self):
         return self.name
+
+class Prediction(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    result = models.CharField(max_length=100)
+    date_predicted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.result
+    
+class RiskAssessment(models.Model):
+    age = models.IntegerField(max_length=3, null=True)
+    gender = models.CharField(max_length=10, null=True)
+    cholesterol = models.IntegerField(max_length=3, null=True)
+    blood_pressure = models.IntegerField(max_length=3, null=True)
+    heart_rate = models.IntegerField(max_length=3, null=True)
+    smoking = models.CharField(max_length=10, null=True)
+    alcohol = models.CharField(max_length=10, null=True)
+    diabetes = models.CharField(max_length=10, null=True)
+    family_history = models.CharField(max_length=10, null=True)
+    obesity = models.CharField(max_length=10, null=True)
+    stress = models.CharField(max_length=10, null=True)
+
+    def __str__(self):
+        return self.result
+
