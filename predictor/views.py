6153929d607989eff9ef8f1f5c7dd1a409d08f37
Victor Mwandike <mwandikev@gmail.com>
2025-02-28 10:49:47 +0300

    Additions

--- predictor/views.py
+++ predictor/views.py
@@ -1,3 +1,4 @@
+import joblib
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
@@ -26,6 +27,35 @@ def register(response):
 
 def login(response):
     return render(response, "registration/login.html")
+
+
+
+
+knn_model = joblib.load('predictor/knn_model.pkl')
+
+
+def predict(request):
+    if request.method == 'POST':
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+        cholesterol = request.POST.get('cholesterol')
+        blood_pressure = request.POST.get('blood_pressure')
+        heart_rate = request.POST.get('heart_rate')
+        smoking = request.POST.get('smoking')
+        alcohol_intake = request.POST.get('alcohol')
+        exercise_hours = request.POST.get('exercise_hours')
+        diabetes = request.POST.get('diabetes')
+        family_history = request.POST.get('family_history')
+        obesity = request.POST.get('obesity')
+        stress_level = request.POST.get('stress_level')
+        blood_sugar = request.POST.get('blood_sugar')
+        chest_pain_type = request.POST.get('chest_pain_type')
+        exercise_induced_angina = request.POST.get('exercise')
+
+        input_data = [[ age, gender, cholesterol, blood_pressure, heart_rate, smoking, alcohol_intake, exercise_hours, diabetes, family_history, obesity, stress_level, blood_sugar, chest_pain_type, exercise_induced_angina]]
+        prediction = knn_model.predict(input_data)
+        prediction_result = "Heart Disease" if prediction == 1 else "No Heart Disease"
+        return render(request, 'features/predict.html', {'prediction_result': prediction_result})
 
 
 # Create your views here.
