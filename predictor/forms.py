7e92ab2e5c014f7345ef0f1377f4e261ad9aac03
Victor Mwandike <mwandikev@gmail.com>
2025-03-24 09:15:29 +0300

    Added

--- predictor/forms.py
+++ predictor/forms.py
@@ -9,21 +9,3 @@ class CustomRegistrationForm(forms.ModelForm):
     class Meta:
         model = UserRegistration
         fields = ['username', 'email', 'password', 'password2']
-
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        password2 = cleaned_data.get("password2")
-
-        if password and password2 and password != password2:
-            raise forms.ValidationError("Passwords do not match.")
-        
-        return cleaned_data
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.password = make_password(self.cleaned_data['password'])
-        user.password2 = None
-        if commit:
-            user.save()
-        return user
