aef20cfb62a5debcc780b698494bc92e3c89b62e
Victor Mwandike <mwandikev@gmail.com>
2025-03-29 07:08:50 +0300

    Added

--- predictor/views.py
+++ predictor/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .forms import CustomRegistrationForm
@@ -14,6 +15,7 @@ from sklearn.preprocessing import StandardScaler
 def home(request):
     return render(request, "features/home.html")
 
+@login_required
 def risk_assessment(request):
     return render(request, "features/risk_assessment.html")
 
@@ -35,6 +37,19 @@ def register(request):
     return render(request, "registration/register.html", {"form":form})
 
 def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully logged in.")
+            return redirect("/home")
+            
+        else:
+            messages.error(request, "Invalid username or password.")
+    
+
     return render(request, "registration/login.html")
 
 
@@ -48,6 +63,7 @@ print("KNN model type:", type(knn_model))
 print("Scaler mean:", scaler.mean_)  
 print("Scaler std:", scaler.scale_)  
 
+@login_required
 def prediction_result(request):
     if request.method == 'POST':
         try:
